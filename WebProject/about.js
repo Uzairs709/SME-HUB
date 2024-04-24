335216c2a4f89b9efda88543560ac1064fce2906
Muhammad Khizer <muhammadkhizer6662@gmail.com>
2024-04-24 10:27:38 +0500

    nav bar

--- WebProject/about.js
+++ WebProject/about.js
@@ -10,9 +10,22 @@ function updateHeadingBasedOnWidth() {
     headingElement.textContent = "Welcome To SME HUB";
   }
 }
+const toggleButton = document.getElementById('toggleButton');
+  const myFooter = document.getElementById('myFooter');
+
+  // Add event listener to the button
+  toggleButton.addEventListener('click', function() {
+    // Toggle the visibility of the footer
+    if (myFooter.style.display === 'none') {
+      myFooter.style.display = 'block';
+    } else {
+      myFooter.style.display = 'none';
+    }
+  });
 
 // Initial call to update heading based on current screen width
 updateHeadingBasedOnWidth();
 
 // Event listener to update heading if screen width changes
 window.addEventListener("resize", updateHeadingBasedOnWidth);
+
