500139df6042c9e89f34b699ad55c7091907222e
Muhammad Khizer <muhammadkhizer6662@gmail.com>
2024-04-29 10:11:48 +0500

    Statistics page

---
+++ WebProject/stats.js
@@ -0,0 +1,86 @@
+const ctx = document.getElementById("revenues");
+
+Chart.defaults.color = "#FFF";
+Chart.defaults.font.family = "Open Sans";
+
+new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    datasets: [
+      {
+        label: "Investment (million [Rs])",
+        data: [
+          5.2, 7.8, 12.3, 11.2, 8.5, 15.8, 13.5, 20, 22.2, 18.7, 29.5, 38,
+        ],
+        backgroundColor: "#F4BD50",
+        borderRadius: 6,
+        borderSkipped: false,
+      },
+    ],
+  },
+  // continuation
+
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: true,
+        text: "Investments in Startups Year 2023",
+        padding: {
+          bottom: 16,
+        },
+        font: {
+          size: 16,
+          weight: "normal",
+        },
+      },
+      tooltip: {
+        backgroundColor: "#27292D",
+      },
+    },
+    scales: {
+      x: {
+        border: {
+          dash: [2, 4],
+        },
+        grid: {
+          color: "#27292D",
+        },
+        title: {
+          text: "2023",
+        },
+      },
+      y: {
+        grid: {
+          color: "#27292D",
+        },
+        border: {
+          dash: [2, 4],
+        },
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: "Investment (million [Rs])",
+        },
+      },
+    },
+  },
+});
