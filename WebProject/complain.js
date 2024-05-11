799e0db0ce6bb512410a1725f46fed2fedceaff6
Muhammad Khizer <muhammadkhizer6662@gmail.com>
2024-05-11 22:58:54 +0500

    sidebars and dropdown attatched

--- WebProject/complain.js
+++ WebProject/complain.js
@@ -1,16 +1,4 @@
-const toggleButton = document.getElementById('toggleButton');
-  const myFooter = document.getElementById('myFooter');
-
-  // Add event listener to the button
-  toggleButton.addEventListener('click', function() {
-    // Toggle the visibility of the footer
-    if (myFooter.style.display === 'none') {
-      myFooter.style.display = 'block';
-    } else {
-      myFooter.style.display = 'none';
-    }
-  });
-  function openSidebar() {
+function openSidebar() {
     const sidebar = document.getElementById("sidebar");
     if (sidebar.style.display === "none") {
       sidebar.style.display = "block"; // Show the sidebar if hidden
