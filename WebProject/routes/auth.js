df78d7d4526c1902376bde3baff2de354d26f462
Uzair Shahid <uzairu809@gmail.com>
2024-05-11 20:08:38 +0500

    get post

--- WebProject/routes/auth.js
+++ WebProject/routes/auth.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello g");
+  res.send("Hello g from auth");
+});
+
+router.post("/signup", (req, res) => {
+  console.log(req.body.name);
 });
 
 module.exports = router;
